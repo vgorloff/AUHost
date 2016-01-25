ac724a0532c85f8a012d7d20669c3b54c0264f89
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2016-01-25 21:18:04 +0200

    [code:≈+] AU plugin view now controls DSP kernel properties.

--- AUSamplePlugIn/AttenuatorKit/AttenuatorAudioUnit.swift
+++ AUSamplePlugIn/AttenuatorKit/AttenuatorAudioUnit.swift
@@ -11,8 +11,6 @@ import AVFoundation
 
 public final class AttenuatorAudioUnit: AUAudioUnit {
 
-	private typealias SampleType = Float32
-
 	public enum Error: ErrorType {
 		case StatusError(OSStatus)
 	}
@@ -23,6 +21,7 @@ public final class AttenuatorAudioUnit: AUAudioUnit {
 	private var _inputBus: AUAudioUnitBus!
 	private var _outputBus: AUAudioUnitBus!
 	private var _pcmBuffer: AVAudioPCMBuffer?
+	private var _dsp = AttenuatorDSPKernel()
 
 	// MARK: -
 	override public var parameterTree: AUParameterTree? {
@@ -40,7 +39,7 @@ public final class AttenuatorAudioUnit: AUAudioUnit {
 			guard status == noErr else {
 				return status
 			}
-			return s.processInputBufferList(buffer, outputBufferList: outputData, frameCount: frameCount)
+			return s._dsp.processInputBufferList(buffer, outputBufferList: outputData, frameCount: frameCount)
 		}
 	}
 
@@ -66,6 +65,7 @@ public final class AttenuatorAudioUnit: AUAudioUnit {
 			throw Error.StatusError(kAudioUnitErr_FailedInitialization)
 		}
 		_pcmBuffer = AVAudioPCMBuffer(PCMFormat: _inputBus.format, frameCapacity: maximumFramesToRender)
+		_dsp.reset()
 	}
 
 	public override func deallocateRenderResources() {
@@ -96,11 +96,13 @@ public final class AttenuatorAudioUnit: AUAudioUnit {
 			let param = AttenuatorParameter.fromRawValue(param.address)
 			return param.stringFromValue(paramValue)
 		}
-		tree.implementorValueObserver = { param, value in
-		// FIXME: set value to kernel. By Vlad Gorlov, Jan 22, 2016.
+		tree.implementorValueObserver = { [weak self] param, value in guard let s = self else { return }
+			let param = AttenuatorParameter.fromRawValue(param.address)
+			return s._dsp.setParameter(param, value: value)
 		}
-		tree.implementorValueProvider = { param in
-			return 100 // FIXME: Use value from kernel. By Vlad Gorlov, Jan 22, 2016.
+		tree.implementorValueProvider = { [weak self] param in guard let s = self else { return AUValue() }
+			let param = AttenuatorParameter.fromRawValue(param.address)
+			return s._dsp.getParameter(param)
 		}
 		return tree
 	}
@@ -108,7 +110,7 @@ public final class AttenuatorAudioUnit: AUAudioUnit {
 	private final func prepareInputBuffer(buffer: AVAudioPCMBuffer, frameCount: AUAudioFrameCount) {
 		let mbl = buffer.mutableAudioBufferList
 		let abl = buffer.audioBufferList
-		let byteSize = frameCount * UInt32(sizeof(SampleType.self))
+		let byteSize = frameCount * UInt32(sizeof(AttenuatorDSPKernel.SampleType.self))
 		mbl.memory.mNumberBuffers = abl.memory.mNumberBuffers
 		let mblPointer = UnsafeMutableAudioBufferListPointer(mbl)
 		let ablPointer = UnsafeMutableAudioBufferListPointer(UnsafeMutablePointer<AudioBufferList>(abl))
@@ -120,36 +122,5 @@ public final class AttenuatorAudioUnit: AUAudioUnit {
 			mB.mDataByteSize = byteSize
 		}
 	}
-
-	private final func processInputBufferList(inAudioBufferList: UnsafeMutablePointer<AudioBufferList>,
-		outputBufferList: UnsafeMutablePointer<AudioBufferList>, frameCount: AVAudioFrameCount) -> AUAudioUnitStatus {
-
-			// 2. Now we have PCM buffer filled with some data. Lets process it.
-			// Number of input and output buffers in our case is equal.
-			let blI = UnsafeMutableAudioBufferListPointer(inAudioBufferList)
-			let blO = UnsafeMutableAudioBufferListPointer(outputBufferList)
-			for index in 0 ..< blO.count {
-				let bO = blO[index]
-				let bI = blI[index]
-				if bO.mData == nil { // Might be a nil
-					bO.mData.memory = bI.mData.memory
-					assert(false)
-				}
-				// We are expecting one buffer per channel.
-				assert(bI.mNumberChannels == bO.mNumberChannels && bI.mNumberChannels == 1)
-				assert(bI.mDataByteSize == bO.mDataByteSize)
-				let samplesBI = UnsafePointer<SampleType>(bI.mData)
-				let samplesBO = UnsafeMutablePointer<SampleType>(bO.mData)
-				// Now we chould copy the data.
-				let numSamples = Int(bO.mDataByteSize / UInt32(sizeof(SampleType.self)))
-				let samplesI = UnsafeBufferPointer<SampleType>(start: samplesBI, count: numSamples)
-				let samplesO = UnsafeMutableBufferPointer<SampleType>(start: samplesBO, count: numSamples)
-				for sampleIndex in 0 ..< samplesI.count {
-					samplesO[sampleIndex] = 0.3 * samplesI[sampleIndex]
-				}
-			}
-			return noErr
-	}
-
 
 }
--- AUSamplePlugIn/AttenuatorKit/AttenuatorViewController.swift
+++ AUSamplePlugIn/AttenuatorKit/AttenuatorViewController.swift
@@ -10,7 +10,22 @@ import CoreAudioKit
 import AVFoundation
 
 public class AttenuatorViewController: AUViewController, AUAudioUnitFactory {
-	var audioUnit: AUAudioUnit!
+	private var audioUnit: AUAudioUnit? {
+		didSet {
+			dispatch_async(dispatch_get_main_queue()) { [weak self] in guard let s = self else { return }
+				if s.viewLoaded {
+					s.connectViewWithAU(s.audioUnit)
+				}
+			}
+		}
+	}
+	private var parameterObserverToken: AUParameterObserverToken?
+	private var parameterGain: AUParameter?
+	private var auView: AttenuatorView! {
+		return view as! AttenuatorView
+	}
+
+	// MARK: - Private
 
 	public override func loadView() {
 		var topLevelObjects: NSArray?
@@ -20,7 +35,7 @@ public class AttenuatorViewController: AUViewController, AUAudioUnitFactory {
 				fatalError()
 		}
 		for object in objects {
-			if let v = object as? NSView {
+			if let v = object as? AttenuatorView {
 				view = v
 				return
 			}
@@ -30,17 +45,34 @@ public class AttenuatorViewController: AUViewController, AUAudioUnitFactory {
 
 	public override func viewDidLoad() {
 		super.viewDidLoad()
-
-		if audioUnit == nil {
-			return
+		connectViewWithAU(audioUnit)
+		auView.handlerParameterDidChaned = {[weak self] parameter, value in guard let s = self else { return }
+			guard let token = s.parameterObserverToken else {
+				return
+			}
+			s.parameterGain?.setValue(value, originator: token)
 		}
-
-		// Get the parameter tree and add observers for any parameters that the UI needs to keep in sync with the AudioUnit
 	}
 
 	public func createAudioUnitWithComponentDescription(componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
-		audioUnit = try AttenuatorAudioUnit(componentDescription: componentDescription, options: [])
-		return audioUnit
+		let au = try AttenuatorAudioUnit(componentDescription: componentDescription, options: [])
+		audioUnit = au
+		return au
 	}
 
+	// MARK: - Private
+
+	private func connectViewWithAU(au: AUAudioUnit?) {
+		guard let paramTree = au?.parameterTree else { return }
+		parameterGain = paramTree.valueForKey(AttenuatorParameter.Gain.parameterID) as? AUParameter
+		parameterObserverToken = paramTree.tokenByAddingParameterObserver { address, value in
+			dispatch_async(dispatch_get_main_queue()) { [weak self] in guard let s = self else { return }
+				let paramType = AttenuatorParameter.fromRawValue(address)
+				s.auView.updateParameter(paramType, withValue: value)
+			}
+		}
+		if let param = parameterGain {
+			auView.updateParameter(AttenuatorParameter.Gain, withValue: param.value)
+		}
+	}
 }
