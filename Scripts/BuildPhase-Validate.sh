663fec9e7a7b394794a3ee4c4f146ef60e2a87ea
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2016-09-19 00:07:23 +0200

    Project: Validation build scripts.

--- Scripts/BuildPhase-Validate.sh
+++ Scripts/BuildPhase-Validate.sh
@@ -3,5 +3,7 @@
 AWLSrcDirPath=$(cd "$(dirname "$0")/../"; pwd)
 cd "$AWLSrcDirPath"
 
-"$AWLSrcDirPath/Shared/Conf/Scripts/BuildPhase_SwiftLint.sh" "$AWLSrcDirPath"
-"$AWLSrcDirPath/Shared/Conf/Scripts/BuildPhase_CheckHeaders.sh" "WaveLabs" "$AWLSrcDirPath/Tests" "$AWLSrcDirPath/Shared" "$AWLSrcDirPath/Sources"
+"$AWLSrcDirPath/Shared/Conf/Scripts/BuildPhase_SwiftLint.sh" "$SRCROOT/AUHost"
+"$AWLSrcDirPath/Shared/Conf/Scripts/BuildPhase_SwiftLint.sh" "$SRCROOT/Shared"
+"$AWLSrcDirPath/Shared/Conf/Scripts/BuildPhase_CheckHeaders.sh" "AUHost" "$SRCROOT/AUHost"
+"$AWLSrcDirPath/Shared/Conf/Scripts/BuildPhase_CheckHeaders.sh" "WaveLabs" "$SRCROOT/Shared"
