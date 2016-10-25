d2cfb7b585e4527073b6d3affdcd7882f85e983b
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2016-10-25 06:51:32 +0200

    Code: Removed workaround for logging.

--- AUHost/AUHost-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  AUHost-Bridging-Header.h
-//  AUHost
-//
-//  Created by Vlad Gorlov on 16.01.16.
-//  Copyright © 2016 WaveLabs. All rights reserved.
-//
-
-#import "../Shared/Core/rdar28599032.h"
