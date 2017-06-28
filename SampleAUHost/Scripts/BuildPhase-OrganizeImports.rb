2f9252359bd54c803cbfa62dd6ad98028f7cb74d
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2017-06-28 22:40:33 +0200

    Added environment check before build script execution.

--- SampleAUHost/Scripts/BuildPhase-OrganizeImports.rb
+++ SampleAUHost/Scripts/BuildPhase-OrganizeImports.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 
 require "#{ENV['PWD']}/Vendor/WL/Conf/Scripts/lib/ImportsOrganizer.rb"
+require "#{ENV['PWD']}/Vendor/WL/Conf/Scripts/lib/Tool.rb"
+
+exit(0) if !Tool.verifyEnvironment("Organize Imports")
+
 ImportsOrganizer.new().process("#{ENV['PWD']}/Sources")
 ImportsOrganizer.new().process("#{ENV['PWD']}/Shared")
 ImportsOrganizer.new().process("#{ENV['PWD']}/Vendor/WL")
