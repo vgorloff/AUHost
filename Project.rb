424db276405586205bb564e4f3316c9b1596be38
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2018-10-02 00:48:00 +0200

    Added Host app target.

--- Project.rb
+++ Project.rb
@@ -9,13 +9,26 @@ class Project < AbstractProject
   
   def generate()
     project = XcodeProject.new(projectPath: File.join(@rootDirPath, "Attenuator_.xcodeproj"))
-    target = project.addApp(name: "AUHost", sources: ["Shared", "SampleAUHost"], platform: :osx)
+    auHost = project.addApp(name: "AUHost", sources: ["Shared", "SampleAUHost"], platform: :osx, deploymentTarget: "10.11", buildSettings: {
+      "PRODUCT_BUNDLE_IDENTIFIER" => "ua.com.wavelabs.AUHost"
+    })
+    addSharedSources(project, auHost)
+    
+    attenuator = project.addApp(name: "Attenuator", sources: ["Shared", "SampleAUPlugin/Attenuator", "SampleAUPlugin/AttenuatorKit"], platform: :osx, deploymentTarget: "10.11", buildSettings: {
+      "PRODUCT_BUNDLE_IDENTIFIER" => "ua.com.wavelabs.Attenuator"
+    })
+    addSharedSources(project, attenuator)
+
+    project.save()
+  end
+  
+  def addSharedSources(project, target)
     project.useFilters(target: target, filters: [
       "Core/Concurrency/*", "Core/Extensions/*", "Core/Converters/*Numeric*",
       "Core/Sources/AlternativeValue*", "Core/Sources/*Aliases*",
       "Foundation/os/log/*", "Foundation/Sources/*Info*", "Foundation/Testability/*", "Foundation/ObjectiveC/*", "Foundation/Notification/*",
       "Foundation/Sources/Functions*", "Foundation/Extensions/CG*", "Foundation/Extensions/*Insets*", "Foundation/Extensions/Color*",
-      "Foundation/Sources/Result*", "Foundation/Sources/Math.swift",
+      "Foundation/Sources/Result*", "Foundation/Sources/Math.swift", "Foundation/Extensions/String*",
       "Types/Sources/MinMax*", "Types/Sources/Random*", "Types/Sources/Operators*",
       "Media/Extensions/*", "Media/Sources/Waveform*", "Media/Sources/Media*", "Media/Sources/*Utility*",
       "Media/Sources/*Type*", "Media/Sources/*Buffer*",
@@ -23,7 +36,6 @@ class Project < AbstractProject
       "AppKit/Extensions/*Toolbar*",
       "Media/DSP/*Value*"
     ])
-    project.save()
   end
 
 end
