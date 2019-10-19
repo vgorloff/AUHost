0cbb83a9fa62ef7cdac98e793f4957aa92e2560c
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2019-10-19 23:25:39 +0200

    Configuration update.

--- Project.js
+++ Project.js
@@ -1,35 +1,33 @@
-import * as Path from 'path';
-import * as WL from "wl-scripting";
+const Path = require('path');
+const AbstractProject = require('wl-scripting').AbstractProject;
+const XcodeBuilder = require('wl-scripting').XcodeBuilder;
+const FileSystem = require('wl-scripting').FileSystem;
 
-class Project extends WL.AbstractProject {
-
-   private projectFilePath: string;
-
-   constructor(projectDirPath: string) {
-      super(projectDirPath)
-      this.projectFilePath = Path.join(this.projectDirPath, "Attenuator.xcodeproj")
+class Project extends AbstractProject {
+   constructor(projectDirPath) {
+      super(projectDirPath);
+      this.projectFilePath = Path.join(this.projectDirPath, 'Attenuator.xcodeproj');
    }
 
    actions() {
-      return ["ci", "build", "clean", "test", "release", "verify", "deploy", "archive"]
+      return ['ci', 'build', 'clean', 'test', 'release', 'verify', 'deploy', 'archive'];
    }
 
    build() {
-      let b = new WL.XcodeBuilder(this.projectFilePath)
-      b.build("AUHost-macOS")
-      b.build("Attenuator-macOS")
+      let b = new XcodeBuilder(this.projectFilePath);
+      b.build('AUHost-macOS');
+      b.build('Attenuator-macOS');
    }
 
    clean() {
-      WL.FileSystem.rmdirIfExists("/tmp/Attenuator.dst")
-      WL.FileSystem.rmdirIfExists(`${this.projectDirPath}/DerivedData`)
-      WL.FileSystem.rmdirIfExists(`${this.projectDirPath}/Build`)
+      FileSystem.rmdirIfExists('/tmp/Attenuator.dst');
+      FileSystem.rmdirIfExists(`${this.projectDirPath}/DerivedData`);
+      FileSystem.rmdirIfExists(`${this.projectDirPath}/Build`);
    }
 
    ci() {
-      let b = new WL.XcodeBuilder(this.projectFilePath)
-      b.ci("AUHost-macOS")
-      b.ci("Attenuator-macOS")
+      let b = new XcodeBuilder(this.projectFilePath);
+      b.ci('AUHost-macOS');
+      b.ci('Attenuator-macOS');
    }
-
 }
