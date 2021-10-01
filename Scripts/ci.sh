8b39f4f57bb0aab0f755155f66cb5b4905d433af
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2021-10-01 19:56:33 +0200

    Attempt to fix CI build.

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -8,5 +8,5 @@ source $(dirname "$0")/common.sh
 SrcDirPath=$(cd "$(dirname "$0")/../"; pwd)
 cd "$SrcDirPath"
 
-xcodebuild -project "$AppProjectFilePath" -scheme "AUHost" CODE_SIGNING_REQUIRED=NO CODE_SIGN_STYLE=Manual DEVELOPMENT_TEAM= CODE_SIGN_IDENTITY= build | xcpretty || exit 1
+xcodebuild -quiet -project "$AppProjectFilePath" -scheme "AUHost" CODE_SIGNING_REQUIRED=NO CODE_SIGN_STYLE=Manual DEVELOPMENT_TEAM= CODE_SIGN_IDENTITY= build || exit 1
 
